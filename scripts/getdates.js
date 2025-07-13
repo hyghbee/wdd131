205521b79726b1f60e9b230db0a5e2b2b828c8d9
hyghbee <highbee700@gmail.com>
2025-07-13 15:21:18 +0100

    commiting all

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// get current year
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
+
+// get last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
